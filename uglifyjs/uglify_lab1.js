1f7826d4b225cc7d3ed094646b2414022e18870e
silverknight3x <silverknight3x@gmail.com>
2018-10-15 13:15:59 +0800

    change new

--- uglifyjs/uglify_lab1.js
+++ uglifyjs/uglify_lab1.js
@@ -4,3 +4,8 @@ var object1 ={
 };
 object1.foo +=object1.bar
 console.log(object1.foo);
+
+
+// uglifyjs uglify_lab1.js
+// uglifyjs uglify_lab1.js -c -m --mangle-props
+// uglifyjs uglify_lab1.js -c -m --mangle-props reserved=[foo,bar]
